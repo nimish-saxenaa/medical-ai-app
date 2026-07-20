4a47ee8172a6b98ea628080d6a81683381d59e53
apple <ravi.kumar@kuvaka.io>
2026-07-20 14:29:38 +0530

    update podfile

--- lib/Custom Widgets/logo_text.dart
+++ lib/Custom Widgets/logo_text.dart
@@ -14,12 +14,14 @@ class LogoAndText extends StatelessWidget {
           children: [
             Image(
               image: const AssetImage('assets/kuvaka_logo.png'),
-              width: width / 11,
+              width: width / 6,
             ),
             const SizedBox(height: 8),
             Text(
               "Clinical AI platform by Kuvaka".toUpperCase(),
-              style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w600),
+              style: Theme.of(
+                context,
+              ).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w600),
             ),
           ],
         ),
